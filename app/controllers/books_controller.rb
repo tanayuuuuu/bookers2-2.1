cd78e1aa67d43dffa78c4d9638cac03e87febedf
tanayuuuuu <101308377+tanayuuuuu@users.noreply.github.com>
2022-05-27 03:55:54 +0000

    応用課題5　提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
 
-  has_many :relationships, foreign_key: :follower_id
+  has_many :relationships, foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :relationships, source: :followed
 
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   has_many :followeds, through: :reverse_of_relationships, source: :follower
 
   has_one_attached :profile_image
@@ -25,4 +25,23 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backword_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "pertial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
+
+
+
+
 end
